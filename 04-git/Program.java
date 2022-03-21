09118a88dc930a274a31b9d59de5f951f71b3474
Bognanna <bakilanowska@gmail.com>
2022-03-21 21:45:44 +0100

    First change

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,4 +1,9 @@
-    //Hello Java
+//Change 1 again again
+//Another one comment for Task 6    
+//Change 2 again again
+//Hello Java
+    //First change
+    //Change 3 again again
     class Simple{  
         public static void main(String args[]){  
          System.out.println("Hello Java");  
